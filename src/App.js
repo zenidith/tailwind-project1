4939b55ce48172adf0e7f781e8c2ba65165c9308
K Sheenan <kaynesheenan+git@gmail.com>
2021-09-29 14:33:20 +0900

    deleted test app, ready to start

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 
 function App() {
   return (
-  <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-    <div>
-      <div className="text-xl font-medium text-primary">ChitChat</div>
-      <p className="text-blue-450">You have a new message!</p>
-    </div>
+  <div className="container">
   </div>
   );
 }
+
+function 
 
 export default App;
